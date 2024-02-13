4120aff655935a4f68de47ade5f78e68f936d818
Karoly Gossler <connor@connor.hu>
2024-02-13 11:52:56 +0100

    Added Test suffix to filename

--- tests/Query/Postgresql/AgeTest.php
+++ tests/Query/Postgresql/AgeTest.php
@@ -4,7 +4,7 @@ namespace DoctrineExtensions\Tests\Query\Postgresql;
 
 use DoctrineExtensions\Tests\Query\PostgresqlTestCase;
 
-class Age extends PostgresqlTestCase
+class AgeTest extends PostgresqlTestCase
 {
     public function testAge(): void
     {

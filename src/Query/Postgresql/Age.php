5aeac272eb3b20b55f0424999b10b2f6d09e4037
Karoly Gossler <connor@connor.hu>
2024-02-13 11:48:54 +0100

    Added ORM 2.19.x || 3.x TokenType class

--- src/Query/Postgresql/Age.php
+++ src/Query/Postgresql/Age.php
@@ -3,9 +3,9 @@
 namespace DoctrineExtensions\Query\Postgresql;
 
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
-use Doctrine\ORM\Query\Lexer;
 use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\TokenType;
 
 /**
  * AgeFunction ::= "AGE" "(" ArithmeticPrimary "," ArithmeticPrimary ")"
@@ -23,13 +23,13 @@ class Age extends FunctionNode
 
     public function parse(Parser $parser): void
     {
-        $parser->match(Lexer::T_IDENTIFIER);
-        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $parser->match(TokenType::T_IDENTIFIER);
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);
 
         $this->date1 = $parser->ArithmeticPrimary();
-        $parser->match(Lexer::T_COMMA);
+        $parser->match(TokenType::T_COMMA);
         $this->date2 = $parser->ArithmeticPrimary();
 
-        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
     }
 }
